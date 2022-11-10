746e8ac109e06b77e513fdcaab9f25484bf57982
Jagannath <you@example.com>
2022-11-10 10:24:46 +0530

    initial commit

---
+++ src/test/java/First_Group/First_A/HelloTest.java
@@ -0,0 +1,13 @@
+package First_Group.First_A;
+
+import org.testng.annotations.Test;
+
+public class HelloTest {
+
+	@Test
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
